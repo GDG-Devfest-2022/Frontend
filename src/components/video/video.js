64dc94b0f162a69b203e209e991b753eb3a92624
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2022-12-03 04:07:05 +0900

    Update video.js

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,10 +1,15 @@
 import './video.css';
 import CHAT from './chat/chat.js';
+import ReactPlayer from 'react-player'
 
 function VIDEO() {
   return (
     <main id="video-rendering">
-      <CHAT />
+    <ReactPlayer
+    width='80vw'
+    height='100vh'
+    url='https://youtu.be/FJfwehhzIhw'  playing   controls/>
+      <CHAT/>
     </main>
   );
 }
