c513c39518bcdb65a5cb9fec751754e02ef14234
daveg7lee <daveg7lee@gmail.com>
2022-12-03 03:28:40 +0900

    socket io set up

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import router from "./router";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 
+export const socket = io("http://localhost:4000/chat");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- src/routes/RoomDetail.jsx
+++ src/routes/RoomDetail.jsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+
 export default function RoomDetail() {
+  useEffect(() => {
+    const roomListHandler = (rooms) => {
+      console.log(rooms);
+      setRooms(rooms);
+    };
+    socket.emit("room-list", roomListHandler);
+
+    return () => {
+      socket.off("room-list", roomListHandler);
+    };
+  }, []);
+
   return <div></div>;
 }
